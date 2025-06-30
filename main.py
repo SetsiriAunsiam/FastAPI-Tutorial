00677db3193d40f9de7deb379d4680c850811700
Setsiri <setsiriaunsiam@gmail.com>
2025-06-30 17:12:25 +0700

    add tax calculate in item and how to use pydantic Model

--- main.py
+++ main.py
@@ -26,4 +26,8 @@ async def read_user(user_id: str):
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
