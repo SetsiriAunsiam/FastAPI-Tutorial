f017572b9724cfc3790f0b5f03b73d272b40f9a6
Setsiri <setsiriaunsiam@gmail.com>
2025-06-30 16:43:31 +0700

    user by id

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ async def root():
 async def read_user_me():
     return {"user_id": "the current user"}
 
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
