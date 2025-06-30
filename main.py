ed0fc232d68b739e9c8b3879b7bc644131df05d3
Setsiri <setsiriaunsiam@gmail.com>
2025-06-30 16:12:17 +0700

    add get user for current user

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
